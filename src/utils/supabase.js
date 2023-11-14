7bc71829b1156c74c58bbb8cb3ce77fa4c20acf1
Nick Talledo <nicolastalledo13@gmail.com>
2023-11-14 02:05:35 -0500

    hide key

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://tukwpgdbyjzczfidigwx.supabase.co";
 // eslint-disable-next-line no-undef
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1a3dwZ2RieWp6Y3pmaWRpZ3d4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODg3NjYxMSwiZXhwIjoyMDE0NDUyNjExfQ.cv1MN9jpDGyKefsfLfGQX38IWmWSMDmckK4sWSuP4BM";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
